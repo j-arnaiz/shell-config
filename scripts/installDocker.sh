1c6fa7538301f28983fbb6831dd4c933743338c4
Jesus Arnaiz <j.arnaiz@gmail.com>
2015-12-05 20:08:53 +0100

    Added install docker script

---
+++ scripts/installDocker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install -y linux-image-extra-$(uname -r) curl docker-engine
+sudo usermod -aG docker `whoami`
+curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmox +x /usr/local/bin/docker-compose
